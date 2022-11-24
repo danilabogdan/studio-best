3b9826b8f5dc65b4e0f435cd74ba242dc59eb0ce
danila bogdan <danilabogdan2018@gmail.com>
2022-11-24 18:31:33 +0200

    Added onScrollEvent on navbar,sideMenu and menuBtn
    Did this so i can hide the menu if the user scrolls down

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ menuBtn.onclick = function() {
         menu.src = "images/menu.png"
     }
 }
+
+
+
+const sideScrollTop = 0;
+    navbar = document.getElementById("sideNav");
+    window.addEventListener("scroll", function(){
+        const scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            sideNav.style.right="-250px";
+        } else {
+            sideNav.style.right="-250px"
+        }
+        lastScrollTop = scrollTop;
+    })
+
+const onScrollTop = 0;
+    navbar = document.getElementById("menuBtn");
+    window.addEventListener("scroll", function(){
+        const scrollTop = window.pageXOffset || this.document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            menuBtn.style.top="-50px";
+        } else {
+            menuBtn.style.top="20px"
+        }
+        lastScrollTop = scrollTop;
+    })
+
+
+
+const lastScrollTop = 0;
+    navbar = document.getElementById("navbar");
+    window.addEventListener("scroll", function(){
+        const scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top="-80px";
+        } else {
+            navbar.style.top="0"
+        }
+        lastScrollTop = scrollTop;
+    })
